3ed8e3eec3f71f825ca90ac4d94169b0ac2b2f77
cliffmax85 <cliffmax85@gmail.com>
2022-04-27 14:37:19 -0700

    test to make sure im hitting the mock server

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -13,7 +13,7 @@ const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: 'Vonta',
+  name: 'CLIFF',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -29,16 +29,18 @@ const server = setupServer(
 );
 // 🚨 Listen for server start
 beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
 
 // 🚨 Close server when complete
 afterAll(() => server.close());
 
 test.only('Should render the header', async () => {
   render(<App />)
+  console.log('||||////CLIFFF', user.name);
+
   const banner = screen.getByRole('banner')
   const headerImg = screen.getByAltText(/alchemy/i)
   const profileName = await screen.findByText(user.name)
-
   expect(banner).toHaveStyle({
     background: 'var(--grey)',
   })
@@ -59,7 +61,7 @@ test('Should render the header with Sasuke 🌬️🔥', async () => {
   }
 
   // 🚨 Use the server to change the response for this test
-
+  
   render(<App />)
 
   const profileName = await screen.findByText(sasuke.name)
